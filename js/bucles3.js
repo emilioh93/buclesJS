4c4ae13a67600f8b0355c9626e6df132142026b7
Emilio Hurtado <emiliohurtado1@gmail.com>
2020-12-16 10:18:37 -0300

    completé ejercicio 3 y comienzo el 4

--- js/bucles3.js
+++ js/bucles3.js
@@ -1,11 +1,8 @@
  /* <p>Realiza un script que pida cadenas de texto hasta que se pulse “cancelar”. Al salir con “cancelar” deben mostrarse todas las cadenas concatenadas con un guión -. Nota: usar confirm() https://www.w3schools.com/jsref/met_win_confirm.asp</p> */
- let mensaje = prompt("Ingrese una cadena de texto");
- let cadena = confirm(mensaje);
+ //  let mensaje = prompt("Ingrese una cadena de texto");
+ //  let cadena = confirm(mensaje);
 
- while (cadena == true) {
-     if (cadena == true) {
-         document.write(mensaje + "<br>");
-     } else {
-         document.write("Apretaste cancelar");
-     }
+ while (confirm("Pulse Aceptar para ingresar una cadena de texto o Cancelar para salir")) {
+     let mensaje = prompt("Ingrese una cadena de texto");
+     document.write(mensaje + " - ");
  }
