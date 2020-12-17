6d205bd7c550846cff6798de88d36c09c2970da5
Emilio Hurtado <emiliohurtado1@gmail.com>
2020-12-17 12:18:41 -0300

    terminé ejercicio 8

---
+++ js/bucles8.js
@@ -0,0 +1,9 @@
+let num = parseInt(prompt("Ingrese un número"))
+
+for (let i = 0; i <= num; i++) {
+    for (let repetir = 1; repetir <= i; repetir++) {
+        document.write(repetir)
+
+    }
+    document.write("<br>")
+}
