801d071034d348ee6d1729c6e0ddeda4db623234
Emilio Hurtado <emiliohurtado1@gmail.com>
2020-12-17 09:19:35 -0300

    renegando con ejercicio 7

---
+++ js/bucles7.js
@@ -0,0 +1,16 @@
+let num = parseInt(prompt("Ingrese un número"));
+
+// 1 imprimir número la cantidad de veces que indica su valor
+// 2 restar 1 al número e imprimir la cantidad de veces que indica su valor
+
+if (num >= 0 && num <= 50) {
+
+} else {
+    alert("Ingresaste un número inválido")
+}
+for (let i = num; i >= 1; i--) {
+    for (repetir; repetir >= 1; repetir--) {
+        document.write(i);
+    }
+    document.write("<br>");
+}
