bfcba26f8f37d57023163c17fdda3f88cf2e937c
Emilio Hurtado <emiliohurtado1@gmail.com>
2020-12-17 11:53:19 -0300

    terminé ejercicio 7

--- js/bucles7.js
+++ js/bucles7.js
@@ -1,16 +1,15 @@
 let num = parseInt(prompt("Ingrese un número"));
 
 // 1 imprimir número la cantidad de veces que indica su valor
-// 2 restar 1 al número e imprimir la cantidad de veces que indica su valor
+// 2 decrementar 1 al número e imprimir la cantidad de veces que indica su valor
 
-if (num >= 0 && num <= 50) {
-
+if (num > 0 && num <= 50) {
+    for (let i = num; i >= 1; i--) {
+        for (let repetir = i; repetir >= 1; repetir--) {
+            document.write(i);
+        }
+        document.write("<br>")
+    }
 } else {
     alert("Ingresaste un número inválido")
-}
-for (let i = num; i >= 1; i--) {
-    for (repetir; repetir >= 1; repetir--) {
-        document.write(i);
-    }
-    document.write("<br>");
 }
