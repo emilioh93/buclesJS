19a550c9ac8a5335317f621a3933dc2f594f308b
Emilio Hurtado <emiliohurtado1@gmail.com>
2020-12-28 00:37:27 -0300

    preparando ejercicio 15

--- js/string13y14.js
+++ js/string13y14.js
@@ -1,7 +1,9 @@
+document.write("Ejercicio 13:<br>");
 let texto = prompt("Ejercicio 13 y 14: Ingrese un texto");
 document.write(texto.toUpperCase());
 let longitud = texto.length;
 document.write("<br>");
+document.write("Ejercicio 14:<br>");
 for (let i = 0; i < longitud; i++) {
     let letra = texto.charAt(i);
     if (i == longitud - 1) {
