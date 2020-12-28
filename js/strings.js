4843da5b283fd4e5bbf9188d20031d0331565221
Emilio Hurtado <emiliohurtado1@gmail.com>
2020-12-28 01:40:45 -0300

    terminé ejercicio 17

--- js/strings.js
+++ js/strings.js
@@ -1,11 +1,11 @@
-document.write("Ejercicio 13:<br>");
+document.write("<b>Ejercicio 13:</b><br>");
 let texto = prompt("Ingrese un texto");
 document.write(texto.toUpperCase());
 let longitud = texto.length;
 document.write("<br>");
 let letra;
 
-document.write("Ejercicio 14:<br>");
+document.write("<b>Ejercicio 14:</b><br>");
 for (let i = 0; i < longitud; i++) {
     letra = texto.charAt(i);
     if (i == longitud - 1) {
@@ -16,7 +16,7 @@ for (let i = 0; i < longitud; i++) {
 }
 document.write("<br>");
 
-document.write("Ejercicio 15:<br>");
+document.write("<b>Ejercicio 15:</b><br>");
 texto = texto.toUpperCase();
 let contador = 0;
 for (let j = 0; j < longitud; j++) {
@@ -28,10 +28,31 @@ for (let j = 0; j < longitud; j++) {
 document.write("La frase tiene " + contador + " vocales.");
 document.write("<br>");
 
-document.write("Ejercicio 16:<br>");
+document.write("<b>Ejercicio 16:</b><br>");
 let invertir = "";
 for (let k = 0; k < longitud; k++) {
     letra = texto.charAt(k);
     invertir = letra + invertir;
 }
 document.write(invertir);
+document.write("<br>");
+
+document.write("<b>Ejercicio 17:</b><br>");
+let vocales = ["a", "e", "i", "o", "u"];
+let textoMin = texto.toLowerCase();
+let posicion = 0;
+let finalizar = false;
+
+for (let l = 0; l < longitud; l++) {
+    for (let m = 0; m < vocales.length; m++) {
+        if (vocales[m] == textoMin.charAt(l)) {
+            posicion = l;
+            finalizar = true;
+            break;
+        }
+    }
+    if (finalizar) {
+        break;
+    }
+}
+document.write("La primera vocal está en la posición " + posicion);
